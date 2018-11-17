e1baac6f1d4bd18a50c169094e911780258e207f
Mano Sriram <mano.sriram0@gmail.com>
2018-11-17 11:51:04 +0530

    mac

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const ffmpeg = require("ffmpeg");
 const ytdl = require("ytdl-core");
 const fs = require("fs");
+const path = require("path");
 
 router.get("/", (req, res) => {
   res.render("download");
@@ -13,13 +14,14 @@ router.get("/error", (req, res) => {
 });
 
 router.post("/getYourFile", (req, res) => {
+  var file = fs.createWriteStream("video.mp4");
   const url = req.body.url;
   const n = url.search("youtube");
   if (n !== -1) {
     res.cookie("checkA", 1, { maxAge: 90000000 });
     ytdl(url, {
       filter: format => format.container === "mp4"
-    }).pipe(fs.createWriteStream("video.mp4"));
+    }).pipe;
     res.render("download");
   } else {
     res.status(403).render("error");
@@ -29,12 +31,25 @@ router.post("/getYourFile", (req, res) => {
 router.post("/getVideo", (req, res) => {
   res.cookie("checkA", 0);
   var file = __dirname + "/../video.mp4";
-  res.download(__dirname + "/../video.mp4");
-  // fs.unlink(file, function(err) {
-  //   if (err) return console.log(err);
-  //   console.log("file deleted successfully");
-  // });
+  res.download(__dirname + "/../video.mp4", "video.mp4");
+
+  fs.access(file, err => {
+    if (!err) {
+      fs.unlinkSync(file);
+    }
+  });
+  // res.redirect("remove");
 });
+
+function deleteFile(file) {
+  fs.unlink(file, function(err) {
+    if (err) {
+      console.error(err.toString());
+    } else {
+      console.warn(file + " deleted");
+    }
+  });
+}
 
 module.exports = router;
 //.pipe(fs.createWriteStream("video.mp4"));
